846b35a88a0abd8dd3e9a462defd053281dc5c53
Rhold James <rjsnavarette@gmail.com>
2023-12-26 17:26:05 +0800

    toggle checked and delete todo function

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,24 @@ export default function App() {
         setNewItem("");
     }
 
+    function toggleTodo(id, completed) {
+        setTodos((currentTodos) => {
+            return currentTodos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, completed };
+                }
+
+                return todo;
+            });
+        });
+    }
+
+    function deleteTodo(id) {
+        setTodos((currentTodos) => {
+            return currentTodos.filter((todo) => todo.id !== id);
+        });
+    }
+
     return (
         <>
             <form onSubmit={handleSubmit} className="new-item-form">
@@ -41,10 +59,18 @@ export default function App() {
                                 <input
                                     type="checkbox"
                                     checked={todo.completed}
+                                    onChange={(e) =>
+                                        toggleTodo(todo.id, e.target.checked)
+                                    }
                                 />
                                 {todo.title}
                             </label>
-                            <button className="btn btn-danger">Delete</button>
+                            <button
+                                onClick={() => deleteTodo(todo.id)}
+                                className="btn btn-danger"
+                            >
+                                Delete
+                            </button>
                         </li>
                     );
                 })}
